7d05319fee8b54fa3f2134f8534403fdba9d0515
MoonyDis <apple@macbook-pro.dlink>
2022-08-11 13:48:53 +0300

    image fix

--- src/components/allConsoles/AllConsoles.js
+++ src/components/allConsoles/AllConsoles.js
@@ -6,42 +6,42 @@ export default function AllConsoles () {
         <div className="wrapper">
             <Console
                 title={'SG-1000'}
-                imgUrl={'./img/consoles/Sega-SG-1000.jpg'}
+                imgUrl={require('../../img/consoles/Sega-SG-1000.jpg')}
                 price={130}
             />
             <Console
-                title={'\'Master System\''}
-                imgUrl={'./img/consoles/Sega-Master.jpg'}
+                title={'Master System'}
+                imgUrl={require('../../img/consoles/Sega-Genesis.jpg')}
                 price={150}
             />
             <Console
                 title={'Mega Drive'}
-                imgUrl={'./img/consoles/Sega-Mega-Drive.jpg'}
+                imgUrl={require('../../img/consoles/Sega-Mega-Drive.jpg')}
                 price={170}
             />
             <Console
                 title={'Game Gear'}
-                imgUrl={'./img/consoles/Game-Gear-Handheld.jpg'}
+                imgUrl={require('../../img/consoles/Game-Gear-Handheld.jpg')}
                 price={220}
             />
             <Console
                 title={'Mega CD'}
-                imgUrl={'./img/consoles/Sega-CD.jpg'}
+                imgUrl={require('../../img/consoles/Sega-CD.jpg')}
                 price={210}
             />
             <Console
                 title={'32X'}
-                imgUrl={'./img/consoles/Sega-Saturn.jpg'}
+                imgUrl={require('../../img/consoles/Sega-Saturn.jpg')}
                 price={270}
             />
             <Console
                 title={'Sega Genesis'}
-                imgUrl={'./img/consoles/Sega-Genesis.jpg'}
+                imgUrl={require('../../img/consoles/Sega-Genesis.jpg')}
                 price={350}
             />
             <Console
                 title={'Sega Dreamcast'}
-                imgUrl={'./img/consoles/Sega-Dreamcast.jpg'}
+                imgUrl={require('../../img/consoles/Sega-Dreamcast.jpg')}
                 price={470}
             />
         </div>
